6afbc09bd1b651aa85b7f3de980323dbc52115e0
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-11-12 17:39:18 +0100

    Add comment for clarity

--- packages/@n8n/db/src/repositories/execution.repository.ts
+++ packages/@n8n/db/src/repositories/execution.repository.ts
@@ -1243,6 +1243,8 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 					startedAt: MoreThanOrEqual(startedAt),
 					stoppedAt: LessThanOrEqual(stoppedAt),
 					executionData: {
+						// We do not have a direct relation between parent and sub-executions,
+						// This is a workaround to link them via executionData's parentExecutionId field
 						data: Like(`%"${parentExecutionId}"%`),
 					},
 				},
